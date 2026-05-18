eeb3b256d09ca9d008be68ad940927438a183921
north-ross <northharperross@gmail.com>
2026-05-18 14:11:29 -0700

    WTD as mod
    Added WTD difference as mod in the model and orchard plot with this included

--- DataExtraction/Koster2023/DataExtraction.R
+++ DataExtraction/Koster2023/DataExtraction.R
@@ -1,8 +1,11 @@
 library(dplyr)
-df <- read_csv("Koster_2023_data_2021_Vascular_plant_biomass.csv")
+library(tidyr)
+df <- read.csv("Koster_2023_data_2021_Vascular_plant_biomass.csv")
 
 # Get SR per site and treatment
 df$treat_site <- paste(df$Treatment, df$Site, sep="_")
+df$treat_site_plt <- paste(df$treat_site, df$Plot, sep="_")
+
 df %>%
   group_by(treat_site) %>%  
   summarise(SR = n_distinct(Species), .groups = 'drop')
@@ -37,13 +40,33 @@ table(df$FG)
 
 df$treat_site_FG <- paste(df$treat_site, df$FG, sep="_")
 # Report biomass by FGs and category
-df %>%
-  group_by(treat_site_FG, Plot) %>%
+fg_df <- df %>%
+  group_by(Treatment, Site, FG, Plot) %>%
   summarise(BM = mean(Max_BM), .groups = 'drop') %>%
-  group_by(treat_site_FG) %>%
+  group_by(Treatment, Site, FG) %>%
   summarise(
     mean_BM = mean(BM),
     n_plots = n(),
     sd_BM = sd(BM),
     .groups = 'drop'
+  ) %>%
+  # pivot to treatment as columns
+  pivot_wider(
+    names_from = Treatment,   
+    values_from = c(mean_BM,n_plots, sd_BM)
   )
+
+# Rename sites to match our names
+type_dict = list(
+  "mes" = 'Rich fen', 
+  "oli" = 'Poor fen', 
+  "omb" = 'Bog')
+
+for (i in 1:nrow(fg_df)){
+  for (j in 1:length(type_dict)){
+    if (grepl(names(type_dict[j]), fg_df[i,"Site"])){
+      fg_df[i, "Site"] <- type_dict[[j]]
+    }
+  }
+}
+fg_df
